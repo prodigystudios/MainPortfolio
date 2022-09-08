70fd0ef3d5b5cebf58dd05d56c5588f3f46d4c6b
william ali <william.ali@hotmail.se>
2022-09-08 19:11:23 +0200

    Mobile quiers

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,53 @@
 <template>
-  <router-view/>
+  <router-view />
 </template>
 
 <style>
-  @import url("https://fonts.googleapis.com/css?family=Outfit");
-  @import url("https://fonts.googleapis.com/css?family=Kodchasan");
-  @import url("https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css");
+@import url("https://fonts.googleapis.com/css?family=Outfit");
+@import url("https://fonts.googleapis.com/css?family=Kodchasan");
+@import url("https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css");
 
-  html {
-    overflow-y:scroll;
-    overflow-x: hidden;
-    scroll-snap-type:y mandatory;
-    scroll-behavior: smooth;
-  }
-#app {
-  font-family:Outfit;
+html
+{
+  overflow-y: scroll;
+  overflow-x: hidden;
+  scroll-snap-type: y mandatory;
+  scroll-behavior: smooth;
+}
+
+#app
+{
+  font-family: Outfit;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  color:white;
+  color: white;
 }
 
-body {
+body
+{
   background:
-  linear-gradient(180.06deg, #0A0A32 52.13%, #05051F 99.95%), 
-  linear-gradient(180deg, #05051F 0%, rgba(14, 14, 100, 0.1) 33.48%);  
+    linear-gradient(180.06deg, #0A0A32 52.13%, #05051F 99.95%),
+    linear-gradient(180deg, #05051F 0%, rgba(14, 14, 100, 0.1) 33.48%);
 }
 
-h1 {
+h1
+{
   font-family: Kodchasan;
 }
-p {
+
+p
+{
   font-family: Outfit;
+}
+
+@media screen and (max-width:425px)
+{
+
+  html
+  {
+    scroll-snap-type: none;
+    scroll-behavior: smooth;
+  }
 }
 </style>
 
--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -2,14 +2,14 @@
     <div class="hero-container">
         <transition name="leftSlide">
             <h1 v-if="showHeader">Blivande front-end utvecklare med stort intresse för back-end utveckling
-            <p>Hej och välkomna till min portfolio. <br />
-                Mitt namn är William Ali 29 årig utvecklare från sverige.<br />
-                Har jobbat inom byggbranchen i över 10 och är nu på min resa att byta karriär till utvecklare!<br />
-                Har länge haft intresse för att skapa saker.<br />
-                Började min resa för att jag ville skapa spel och det ledde mig senare in på att jag ville testa
-                på<br />
-                webbutveckling och fastnat för det helt. Både för front- end och back-end utveckling.</p>
-        </h1>
+                <p>Hej och välkomna till min portfolio. <br />
+                    Mitt namn är William Ali 29 årig utvecklare från sverige.<br />
+                    Har jobbat inom byggbranchen i över 10 och är nu på min resa att byta karriär till utvecklare!<br />
+                    Har länge haft intresse för att skapa saker.<br />
+                    Började min resa för att jag ville skapa spel och det ledde mig senare in på att jag ville testa
+                    på<br />
+                    webbutveckling och fastnat för det helt. Både för front- end och back-end utveckling.</p>
+            </h1>
         </transition>
         <transition name="rightSlide">
             <img v-if="showImg" class="img" src="../assets/Portrait/Självporträttlol.jpg">
@@ -23,7 +23,7 @@ export default {
         this.showImg = true;
         this.showHeader = true;
     },
-    data () {
+    data() {
         return {
             showImg: false,
             showHeader: false,
@@ -66,21 +66,67 @@ export default {
 
 
 .rightSlide-enter-active,
-.rightSlide-leave-active {
+.rightSlide-leave-active
+{
     transition: all 1s ease;
 }
+
 .rightSlide-enter-from,
-.rightSlide-leave-to {
-    transform:translateX(100%);
+.rightSlide-leave-to
+{
+    transform: translateX(100%);
     opacity: 0;
 }
+
 .leftSlide-enter-active,
-.leftSlide-leave-active {
+.leftSlide-leave-active
+{
     transition: all 1s ease;
 }
+
 .leftSlide-enter-from,
-.leftSlide-leave-to {
+.leftSlide-leave-to
+{
     transform: translateX(-100%);
     opacity: 0;
+}
+
+
+@media screen and (max-width: 425px)
+{
+
+    .hero-container
+    {
+        display: flex;
+        align-items: center;
+        flex-direction: column-reverse;
+        margin-left: 0;
+        margin-top:80px;
+        margin-bottom: 50px;
+        gap: 50px;
+    }
+
+    .hero-container h1
+    {
+        font-size: 28px;
+        width: 100%;
+    }
+
+    .hero-container p
+    {
+        margin-top: 50px;
+        font-size: 15px;
+        font-weight: 300;
+        line-height: 30px;
+    }
+
+    .img
+    {
+        margin-top: 80px;
+        width: 300px;
+        height: 300px;
+        border-radius: 300px;
+        object-fit: cover;
+    }
 }
 </style>
